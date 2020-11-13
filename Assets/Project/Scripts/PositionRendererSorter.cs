7ffcc24d355552da826561c016af8ba71bf601f7
aishajoomun <hum_1998@live.com>
2020-11-14 03:13:36 +0400

    Added sorting order script

---
+++ Assets/Project/Scripts/PositionRendererSorter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRendererSorter : MonoBehaviour
+{
+    [SerializeField]
+    private int _sortingOrderBase = 5000;
+    private Renderer _renderer;
+    
+    void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _renderer.sortingOrder = (int)(_sortingOrderBase - (transform.position.y * 100f));
+        
+    }
+}
